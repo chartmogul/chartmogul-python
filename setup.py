46562ba4c09042a117c20daebe1a3f4cbff67a68
Matt <matt@goteamup.com>
2017-02-15 03:26:28 -0500

    Fix setup.py to include api and imp subpackages and requirements (#1)

--- setup.py
+++ setup.py
@@ -13,7 +13,13 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-requires = []
+requires = [
+    'requests>=2.10.0',
+    'uritemplate==3.0.0',
+    'promise==1.0.1',
+    'marshmallow==2.12.2',
+    'future==0.16.0',
+]
 test_requirements = ['mock>=1.0.1', 'requests-mock>=1.3.0']
 
 with open('chartmogul/__init__.py', 'r') as fd:
@@ -32,9 +38,9 @@ setup(
     author_email='petr@chartmogul.com',
     url='https://chartmogul.com',
     download_url = github_url + '/tarball/v' + version,
-    packages=['chartmogul'],
+    packages=['chartmogul', 'chartmogul.api', 'chartmogul.imp'],
     package_data={'': ['LICENSE', 'NOTICE'], 'chartmogul': ['*.pem']},
-    package_dir={'chartmogul': 'chartmogul'},
+    package_dir={'chartmogul': 'chartmogul', 'chartmogul.api': 'chartmogul/api', 'chartmogul.imp': 'chartmogul/imp'},
     include_package_data=True,
     install_requires=requires,
     license='MIT',
