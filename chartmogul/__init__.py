0cc6e0751408e896351631c3b75dcecd23fd83cd
pkopac <kopacpetr@gmail.com>
2017-08-03 09:14:51 +0200

    Version 1.1.6

--- chartmogul/__init__.py
+++ chartmogul/__init__.py
@@ -29,7 +29,7 @@ Provides convenient Python bindings for ChartMogul's API.
 """
 
 __title__ = 'chartmogul'
-__version__ = '1.1.5'
+__version__ = '1.1.6'
 __build__ = 0x000000
 __author__ = 'ChartMogul Ltd'
 __license__ = 'MIT'
