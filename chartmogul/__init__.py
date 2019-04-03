b106d4769cdd35230d8c8fd451cf4c0b54d3ccf4
Petr Kopac <kopacpetr@gmail.com>
2019-04-03 15:41:56 +0200

    Version bump

--- chartmogul/__init__.py
+++ chartmogul/__init__.py
@@ -29,7 +29,7 @@ Provides convenient Python bindings for ChartMogul's API.
 """
 
 __title__ = 'chartmogul'
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 __build__ = 0x000000
 __author__ = 'ChartMogul Ltd'
 __license__ = 'MIT'
