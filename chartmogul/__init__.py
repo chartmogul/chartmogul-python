1cd73491b0845dd5ac0374866cc0818bbd96fc55
Srikala Bharadwaj <srikalabharadwaj@Srikalas-MBP.fritz.box>
2021-06-21 12:33:42 +0200

    Bump major version due to breaking changes in Metrics [ch30595]

--- chartmogul/__init__.py
+++ chartmogul/__init__.py
@@ -32,7 +32,7 @@ Provides convenient Python bindings for ChartMogul's API.
 """
 
 __title__ = 'chartmogul'
-__version__ = '1.6.2'
+__version__ = '2.0.0'
 __build__ = 0x000000
 __author__ = 'ChartMogul Ltd'
 __license__ = 'MIT'
