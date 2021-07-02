e8640093a87890ff4df259b4b5582732c08f4021
Srikala Bharadwaj <srikala@chartmogul.com>
2021-07-02 09:18:09 +0200

    Bump patch version [ch30595]

--- chartmogul/__init__.py
+++ chartmogul/__init__.py
@@ -32,7 +32,7 @@ Provides convenient Python bindings for ChartMogul's API.
 """
 
 __title__ = 'chartmogul'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __build__ = 0x000000
 __author__ = 'ChartMogul Ltd'
 __license__ = 'MIT'
