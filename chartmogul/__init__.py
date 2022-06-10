73f81603148bb18b5f9ea7a8150b4e6f1a4903c8
Dina Vojinovic <dina@chartmogul.com>
2022-06-10 10:57:08 +0200

    version bump

--- chartmogul/__init__.py
+++ chartmogul/__init__.py
@@ -37,7 +37,6 @@ Provides convenient Python bindings for ChartMogul's API.
 """
 
 __title__ = 'chartmogul'
-__version__ = '3.0.1'
 __build__ = 0x000000
 __author__ = 'ChartMogul Ltd'
 __license__ = 'MIT'
