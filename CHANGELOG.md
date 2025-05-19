10ada0f9cfb4740ee77011b174abf3f1778a592a
Keith Dawson <keith@chartmogul.com>
2025-05-19 10:56:38 +0900

    [sc-67784] Fixed Tasks API schema issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,9 @@ and this project adheres to [Semantic Versioning].
 [Keep a Changelog]: https://keepachangelog.com/en/1.0.0/
 [Semantic Versioning]: https://semver.org/spec/v2.0.0.html
 
+## [4.6.2] - 2025-05-19
+- Fixed Tasks API schema issue
+
 ## [4.6.1] - unreleased
 - Unify requirements in a single place
 - Support for Python 3.13.
--- test/api/test_task.py
+++ test/api/test_task.py
@@ -5,7 +5,7 @@ import requests_mock
 from pprint import pprint
 
 task = {
-    "uuid": "00000000-0000-0000-0000-000000000000",
+    "task_uuid": "00000000-0000-0000-0000-000000000000",
     "customer_uuid": "cus_00000000-0000-0000-0000-000000000000",
     "assignee": "customer@example.com",
     "task_details": "This is some task details text.",
@@ -80,7 +80,7 @@ class TaskTestCase(unittest.TestCase):
     @requests_mock.mock()
     def test_patch(self, mock_requests):
         new_task = {
-            "uuid": "00000000-0000-0000-0000-000000000000",
+            "task_uuid": "00000000-0000-0000-0000-000000000000",
             "customer_uuid": "cus_00000000-0000-0000-0000-000000000000",
             "assignee": "customer@example.com",
             "task_details": "This is some task details text.",
