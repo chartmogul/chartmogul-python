8e44b1fa91ab73c448c05ecb6ba233d73461335b
Wiktor Plaga <wiktor@chartmogul.com>
2026-04-09 15:54:07 +0200

    Add account ID field support (PIP-120) (#131)

--- test/api/test_account.py
+++ test/api/test_account.py
@@ -35,3 +35,46 @@ class AccountTestCase(unittest.TestCase):
         self.assertEqual(account.currency, "EUR")
         self.assertEqual(account.time_zone, "Europe/Berlin")
         self.assertEqual(account.week_start_on, "sunday")
+
+
+jsonResponseWithId = {
+    "id": "acct_a1b2c3d4",
+    "name": "Example Test Company",
+    "currency": "EUR",
+    "time_zone": "Europe/Berlin",
+    "week_start_on": "sunday",
+}
+
+
+class AccountIdTestCase(unittest.TestCase):
+
+    @requests_mock.mock()
+    def test_retrieve_with_id(self, mock_requests):
+        mock_requests.register_uri(
+            "GET",
+            "https://api.chartmogul.com/v1/account",
+            request_headers={"Authorization": "Basic dG9rZW46"},
+            status_code=200,
+            json=jsonResponseWithId,
+        )
+
+        config = Config("token")
+        account = Account.retrieve(config).get()
+        self.assertTrue(isinstance(account, Account))
+        self.assertEqual(account.id, "acct_a1b2c3d4")
+
+    @requests_mock.mock()
+    def test_retrieve_without_id_field(self, mock_requests):
+        """Old API responses without id field should not break deserialization."""
+        mock_requests.register_uri(
+            "GET",
+            "https://api.chartmogul.com/v1/account",
+            request_headers={"Authorization": "Basic dG9rZW46"},
+            status_code=200,
+            json=jsonResponse,
+        )
+
+        config = Config("token")
+        account = Account.retrieve(config).get()
+        self.assertTrue(isinstance(account, Account))
+        self.assertFalse(hasattr(account, "id"))
