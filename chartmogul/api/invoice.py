23a23720018fa32a8884321b11993913f2c9c7cb
Wiktor Plaga <wiktor@chartmogul.com>
2026-04-10 13:09:19 +0200

    Expose validation errors on LineItem and Transaction schemas (PIP-304) (#133)

--- chartmogul/api/invoice.py
+++ chartmogul/api/invoice.py
@@ -27,6 +27,7 @@ class LineItem(DataObject):
         account_code = fields.String(allow_none=True)
         description = fields.String(allow_none=True)
         event_order = fields.Int(allow_none=True)
+        errors = fields.Dict(allow_none=True)
 
         @post_load
         def make(self, data, **kwargs):
